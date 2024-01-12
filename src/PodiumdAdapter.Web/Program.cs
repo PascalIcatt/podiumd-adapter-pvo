d6c73b1fc674067253042881f2752f8f4f6b0d7c
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-01-12 13:52:28 +0100

    simple logging

--- src/PodiumdAdapter.Web/Program.cs
+++ src/PodiumdAdapter.Web/Program.cs
@@ -4,35 +4,51 @@ using PodiumdAdapter.Web.Auth;
 using PodiumdAdapter.Web.Endpoints;
 using PodiumdAdapter.Web.Infrastructure;
 using Serilog;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+using var logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
+    .WriteTo.Console()
+    .CreateLogger();
 
-// Add services to the container.
-builder.Host.UseSerilog((ctx, lc) => lc
-    .ReadFrom.Configuration(ctx.Configuration)
-    .Enrich.FromLogContext());
-
-builder.Services.AddHealthChecks();
-builder.Services.AddHttpClient();
-builder.Services.AddTransient<ESuiteRequestAdapter>();
-builder.Services.AddTransient(s => new ContactmomentenClient(s.GetEsuiteRequestAdapter("ESUITE_CONTACTMOMENTEN_BASE_URL")));
-builder.Services.AddTransient(s => new KlantenClient(s.GetEsuiteRequestAdapter("ESUITE_KLANTEN_BASE_URL")));
-
-if (!builder.Environment.IsDevelopment())
+try
 {
-    builder.Services.AddAuth(builder.Configuration);
+    logger.Write(LogEventLevel.Information, "Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Host.UseSerilog(logger);
+
+    builder.Services.AddHealthChecks();
+    builder.Services.AddHttpClient();
+    builder.Services.AddTransient<ESuiteRequestAdapter>();
+    builder.Services.AddTransient(s => new ContactmomentenClient(s.GetEsuiteRequestAdapter("ESUITE_CONTACTMOMENTEN_BASE_URL")));
+    builder.Services.AddTransient(s => new KlantenClient(s.GetEsuiteRequestAdapter("ESUITE_KLANTEN_BASE_URL")));
+
+    if (!builder.Environment.IsDevelopment())
+    {
+        builder.Services.AddAuth(builder.Configuration);
+    }
+
+    var app = builder.Build();
+    // Configure the HTTP request pipeline.
+
+    app.UseSerilogRequestLogging();
+
+    app.Map(Contactmomenten.Api);
+    app.Map(Klanten.Api);
+
+    app.MapHealthChecks("/healthz").AllowAnonymous();
+
+    app.Run();
+}
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    logger.Write(LogEventLevel.Fatal, ex, "Application terminated unexpectedly");
 }
 
-var app = builder.Build();
-// Configure the HTTP request pipeline.
-
-app.UseSerilogRequestLogging();
-
-app.Map(Contactmomenten.Api);
-app.Map(Klanten.Api);
-
-app.MapHealthChecks("/healthz").AllowAnonymous();
-
-app.Run();
 
 public partial class Program { }
