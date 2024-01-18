e824a593e9ba06dd02c3c3bd8fd409a0401d5787
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-01-18 10:54:20 +0100

    cleanup

--- src/PodiumdAdapter.Web/Endpoints/KlantenClientConfig.cs
+++ src/PodiumdAdapter.Web/Endpoints/KlantenClientConfig.cs
@@ -10,11 +10,15 @@ namespace PodiumdAdapter.Web.Endpoints
 
         public void MapCustomEndpoints(IEndpointRouteBuilder clientRoot, Func<HttpClient> getClient)
         {
+            // tot het GET request in de API geimplementeerd is, doen we hier een patch met een leeg object als body
             clientRoot.MapGet("/klanten/{id:guid}", (Guid id) =>
             {
                 var client = getClient();
-                var request = () => new HttpRequestMessage(HttpMethod.Patch, "klanten/" + id) { Content = JsonContent.Create(new { }) };
-                return client.ProxyResult(request);
+                HttpRequestMessage CreateRequest() => new (HttpMethod.Patch, "klanten/" + id) 
+                { 
+                    Content = JsonContent.Create(new { }) 
+                };
+                return client.ProxyResult(CreateRequest);
             });
         }
     }
