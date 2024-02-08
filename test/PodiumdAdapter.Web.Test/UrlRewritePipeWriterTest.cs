effac186863dbfe8b1b2e413f3bf31eaa52d75e6
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-02-08 18:23:38 +0100

    ADD more tests for edge cases

--- test/PodiumdAdapter.Web.Test/UrlRewritePipeWriterTest.cs
+++ test/PodiumdAdapter.Web.Test/UrlRewritePipeWriterTest.cs
@@ -7,56 +7,89 @@ namespace PodiumdAdapter.Web.Test
     public class UrlRewritePipeWriterTest
     {
         [Fact]
-        public async Task BasicTest()
+        public async Task When_the_full_url_fits_within_one_buffer()
         {
-            var (read, write) = CreatePipe("with-", "this", "replace-", "me");
+            var output = await UsingPipe("with-", "this", "replace-", "me", async write =>
+            {
+                await write("start-");
+                await write("replace-me");
+                await write("-end");
+            });
 
-            await write("start-replace-me-end");
-
-            var output = await read();
             Assert.Equal("start-with-this-end", output);
         }
 
-        private static (Func<Task<string>> Read, Func<string, Task> Write) CreatePipe(string localRoot, string localPath, string remoteRoot, string remotePath)
+        [Fact]
+        public async Task When_the_base_url_to_replace_is_split_between_buffers()
+        {
+            var output = await UsingPipe("with-", "this", "replace-", "me", async write =>
+            {
+                await write("start-");
+                await write("before-repla");
+                await write("ce-me-after");
+                await write("-end");
+            });
+
+            Assert.Equal("start-before-with-this-after-end", output);
+        }
+
+        [Fact]
+        public async Task When_the_relative_url_to_replace_is_split_between_buffers()
+        {
+            var output = await UsingPipe("with-", "this", "replace-", "me", async write =>
+            {
+                await write("start-");
+                await write("before-replace-m");
+                await write("e-after");
+                await write("-end");
+            });
+
+            Assert.Equal("start-before-with-this-after-end", output);
+        }
+
+        [Fact]
+        public async Task When_the_base_url_is_seperated_from_the_relative_url_between_buffers()
+        {
+            var output = await UsingPipe("with-", "this", "replace-", "me", async write =>
+            {
+                await write("start-");
+                await write("before-replace-");
+                await write("me-after");
+                await write("-end");
+            });
+
+            Assert.Equal("start-before-with-this-after-end", output);
+        }
+
+        delegate ValueTask<FlushResult> Write(string text);
+
+        private static async Task<string> UsingPipe(string localRoot, string localPath, string remoteRoot, string remotePath, Func<Write, Task> test)
         {
             var pipe = new Pipe();
-            var reader = pipe.Reader;
             var replacer = new UrlRewriter(localRoot + localPath, remoteRoot + remotePath);
             var writer = new UrlRewritePipeWriter(pipe.Writer, new(localRoot, remoteRoot, [replacer]));
-            return (() => ReadToEnd(reader), (s) => WriteToEnd(writer, s));
-        }
 
-        private static async Task WriteToEnd(PipeWriter writer, string str)
-        {
-            await writer.WriteAsync(Encoding.UTF8.GetBytes(str));
-            await writer.CompleteAsync();
-        }
-
-        private static async Task<string> ReadToEnd(PipeReader reader)
-        {
-            var stringBuilder = new StringBuilder();
-            while (true)
+            Write write = async (string s) =>
             {
-                var pipeReadResult = await reader.ReadAsync();
-                var buffer = pipeReadResult.Buffer;
+                await Task.Delay(1);
+                return await writer.WriteAsync(Encoding.UTF8.GetBytes(s));
+            };
+            var readTask = Read(pipe.Reader);
+            await test(write);
+            await writer.CompleteAsync();
+            var result = await readTask;
+            await pipe.Reader.CompleteAsync();
+            return result;
+        }
 
-                try
-                {
-                    //process data in buffer
-                    stringBuilder.Append(Encoding.UTF8.GetString(buffer));
-
-                    if (pipeReadResult.IsCompleted)
-                    {
-                        break;
-                    }
-                }
-                finally
-                {
-                    reader.AdvanceTo(buffer.Start, buffer.End);
-                }
-            }
-
-            return stringBuilder.ToString();
+        private static async Task<string> Read(PipeReader reader)
+        {
+            using var memory = new MemoryStream();
+            using var stream = reader.AsStream();
+            await StreamCopierFromYarpSourceCode.CopyAsync(stream, memory, -1, default);
+            memory.Seek(0, SeekOrigin.Begin);
+            using var strReader = new StreamReader(memory);
+            return await strReader.ReadToEndAsync();
         }
     }
 }
