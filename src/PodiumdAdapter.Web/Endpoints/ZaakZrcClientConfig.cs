87acc7ad44c77643a24941ea00fda683abcf1dd0
Felix Cornelissen <felix.cornelissen@icatt.nl>
2024-02-14 16:47:45 +0100

    ADD dummy zaakcontactmomenten endpoint

--- src/PodiumdAdapter.Web/Endpoints/ZaakZrcClientConfig.cs
+++ src/PodiumdAdapter.Web/Endpoints/ZaakZrcClientConfig.cs
@@ -36,6 +36,8 @@ namespace PodiumdAdapter.Web.Endpoints
                     return new ValueTask();
                 }
             }));
+
+            clientRoot.MapPost("/zaakcontactmomenten", () => Results.NoContent());
         }
 
         private static void MapInternZaaknummerToIdentificatie(JsonNode? item)
