e4083730266b53c33a832c3fd73cc7a4cfe39e8c
mark stoker <mark@icatt.nl>
2024-04-04 13:50:27 +0200

    header ook meegeven bij gebruik custom httpclient

--- src/PodiumdAdapter.Web/Program.cs
+++ src/PodiumdAdapter.Web/Program.cs
@@ -23,7 +23,7 @@ try
 
     builder.Services.AddHttpContextAccessor();
     builder.Services.AddHealthChecks();
-    builder.Services.AddReverseProxy().AddEsuiteToken();
+    builder.Services.AddReverseProxy().AddEsuiteToken().AddEsuiteHeader();
     builder.Services.AddESuiteClient(new ContactmomentenClientConfig());
     builder.Services.AddESuiteClient(new KlantenClientConfig());
     builder.Services.AddESuiteClient(new ZaakZrcClientConfig());
@@ -34,10 +34,9 @@ try
     builder.Services.AddSmoelenboekClient(builder.Configuration);
     builder.Services.AddUrlRewriter(EsuiteUrlRewriteMaps.GetRewriters);
 
-    if (!builder.Environment.IsDevelopment())
-    {
-        builder.Services.AddAuth(builder.Configuration);
-    }
+    //comment this line if you want to use PodiumdAdapter.Web.http
+    builder.Services.AddAuth(builder.Configuration);
+
 
     var app = builder.Build();
     // Configure the HTTP request pipeline.
