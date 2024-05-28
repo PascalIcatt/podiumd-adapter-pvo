87142be43bd79ffb12e55ea5790511a8a081d663
githubjimmeicatt <jimme@icatt.nl>
2024-05-28 18:54:26 +0200

    toeveogen status codes dmv custom middleware

---
+++ src/PodiumdAdapter.Web/Middleware/StatusCodeLoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace PodiumdAdapter.Web.Middleware;
+
+public class StatusCodeLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<StatusCodeLoggingMiddleware> _logger;
+
+    public StatusCodeLoggingMiddleware(RequestDelegate next, ILogger<StatusCodeLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await _next(context);
+
+        if (context.Response.StatusCode == 400 ||
+            context.Response.StatusCode == 401 ||
+            context.Response.StatusCode == 403 ||
+            context.Response.StatusCode == 404 ||
+            context.Response.StatusCode == 500)
+        {
+            _logger.LogInformation("HTTP {StatusCode} response: {Path}, TraceIdentifier: {TraceIdentifier}",
+                context.Response.StatusCode, context.Request.Path, context.TraceIdentifier);
+        }
+    }
+}
+
